da9ecb334fed3408451673c35092e1b78d45782d
Ion <teloslinux@protonmail.com>
2024-05-26 18:08:06 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,8 +2,7 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('marko-cache-v1').then(function(cache) {
       return cache.addAll([
-        '/',
-        '/index.html',
+        '/marko',
         // Add other files you want to cache
       ]);
     })
