60e5a71dd8661af374bd712bd8996f5eca23e365
Ion <teloslinux@protonmail.com>
2024-07-05 00:34:33 +0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -17,41 +17,24 @@ firebase.initializeApp(firebaseConfig);
 // Initialize Firebase Messaging
 var messaging = firebase.messaging();
 
-// Handle background messages
-messaging.onBackgroundMessage((payload) => {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
+messaging.onBackgroundMessage(function(payload) {
+  console.log('Received background message ', payload);
+
+  // Customize notification here
+  var notificationTitle = payload.notification.title;
+  var notificationOptions = {
     body: payload.notification.body,
-    icon: 'https://github.com/IonTeLOS/iontelos.github.io/blob/5d1ef458101e5cdd316e5bd0cf73fa7e8bbfed8c/icon.png', // Replace with your app's icon path
-    data: payload.data, // This will contain the FCM data payload
+    data: {
+      url: `https://teloslinux.org/marko/newfile?uuid=${payload.data.uuid}`
+    }
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
 
-// Handle notification click event
-self.addEventListener('notificationclick', (event) => {
-  console.log('[firebase-messaging-sw.js] Notification click Received.');
-
+self.addEventListener('notificationclick', function(event) {
   event.notification.close();
-
-  // This looks to see if the current is already open and focuses if it is
   event.waitUntil(
-    clients.matchAll({ type: 'window', includeUncontrolled: true })
-      .then((clientList) => {
-        const hadWindowToFocus = clientList.some((windowClient) => {
-          if (windowClient.url === event.notification.data.FCM_MSG.webpush.fcm_options.link) {
-            return windowClient.focus();
-          }
-          return false;
-        });
-
-        if (!hadWindowToFocus) {
-          // If a window matching the URL wasn't found, open a new one
-          clients.openWindow(event.notification.data.FCM_MSG.webpush.fcm_options.link);
-        }
-      })
+    clients.openWindow(event.notification.data.url)
   );
 });
