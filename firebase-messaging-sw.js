3ba849d377c5b149e6e1182dc603d6c0a1c77264
Ion <teloslinux@protonmail.com>
2024-07-06 15:11:25 +0300

    Add files via upload

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,60 @@
+importScripts('https://www.gstatic.com/firebasejs/8.6.2/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.6.2/firebase-messaging.js');
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD96IBVqGKVEdmXIVCYL_7kvlBhJNSD1Ww",
+  authDomain: "marko-be9a9.firebaseapp.com",
+  databaseURL: "https://marko-be9a9-default-rtdb.firebaseio.com",
+  projectId: "marko-be9a9",
+  storageBucket: "marko-be9a9.appspot.com",
+  messagingSenderId: "7036670175",
+  appId: "1:7036670175:web:99992356716578ea13996a"
+};
+
+firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
+
+self.addEventListener('install', (event) => {
+  console.log('Service Worker installing.');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker activating.');
+  event.waitUntil(
+    self.clients.claim()
+  );
+});
+
+messaging.onBackgroundMessage(function(payload) {
+  console.log('Received background message ', payload);
+
+  // Check if it's a mobile device
+  if (!('serviceWorker' in navigator) || !('PushManager' in window)) {
+    // This is likely a mobile device, don't show the notification
+    return;
+  }
+
+ const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: payload.notification.icon,
+    data: {
+      url: payload.data.url
+    }
+  };
+
+self.registration.showNotification(notificationTitle, notificationOptions);
+});
+/*
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+
+  const myMarko = event.notification.data.url;
+
+  event.waitUntil(
+    clients.openWindow(myMarko)
+  );
+});
+
+*/
