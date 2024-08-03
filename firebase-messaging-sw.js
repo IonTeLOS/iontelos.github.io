29a26066b9f524ab7ee7422a4596cb899e0a65dd
Ion <teloslinux@protonmail.com>
2024-08-03 17:58:17 +0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -29,45 +29,44 @@ messaging.onBackgroundMessage((payload) => {
     data: payload.data 
   };
 
-  
-//store a value for the redirect to the Marko link to happen when page is opened or focused  
-if (payload.data && payload.data.goto) {  
-localforage.setItem('new-nav-request', String(payload.data.goto)).then(function() {
-  console.log('Navigation request stored successfully in localForage from Service Worker.');
-}).catch(function(err) {
-  console.error('Error storing value in Service Worker:', err);
-});
-}
-if (payload.data && payload.data.uuid) {  
-localforage.setItem('newUnopenedReminder', String(payload.data.path)).then(function() {
-  console.log('Pending reminder stored successfully in localForage from Service Worker.');
-}).catch(function(err) {
-  console.error('Error storing value in Service Worker:', err);
-});
-}
-  
-      // Check if it's a mobile device
+  // Store a value for the redirect to the Marko link to happen when page is opened or focused  
+  if (payload.data && payload.data.goto) {  
+    localforage.setItem('new-nav-request', String(payload.data.goto)).then(function() {
+      console.log('Navigation request stored successfully in localForage from Service Worker.');
+    }).catch(function(err) {
+      console.error('Error storing value in Service Worker:', err);
+    });
+  }
+
+  if (payload.data && payload.data.uuid) {  
+    localforage.setItem('newUnopenedReminder', String(payload.data.path)).then(function() {
+      console.log('Pending reminder stored successfully in localForage from Service Worker.');
+    }).catch(function(err) {
+      console.error('Error storing value in Service Worker:', err);
+    });
+  }
+
+  // Check if it's a mobile device
   if (!('serviceWorker' in navigator) || !('PushManager' in window)) {
     // This is likely a mobile device, don't show the notification but store a value for effective redirect
     return;
   }
-  
+
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
 
 self.addEventListener('notificationclick', function(event) {
   event.notification.close();
-  
+
   let newUrl = 'https://teloslinux.org/marko/newfile';
 
   if (event.notification && event.notification.data.path) {
-      const path = event.notification.data.path;
-      const goUuid = path;
-      newUrl = `https://teloslinux.org/marko/newfile?uuid=${goUuid}`;
+    const path = event.notification.data.path;
+    const goUuid = path;
+    newUrl = `https://teloslinux.org/marko/newfile?uuid=${goUuid}`;
   } else if (event.notification && event.notification.data.goto) {
-      const navUrl = String(goTo);
-      newUrl = `https://teloslinux.org/marko/newfile?nav=${navUrl}`;
-    }
+    const navUrl = String(event.notification.data.goto);
+    newUrl = `https://teloslinux.org/marko/newfile?nav=${navUrl}`;
   }
 
   event.waitUntil(
