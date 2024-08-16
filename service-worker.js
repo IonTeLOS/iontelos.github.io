64e4d5fdffa0fcb2f26465bf46c64638acf6fd36
Ion <teloslinux@protonmail.com>
2024-08-16 22:40:46 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,7 @@
+importScripts('https://www.gstatic.com/firebasejs/8.6.2/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.6.2/firebase-messaging.js');
+importScripts('https://cdnjs.cloudflare.com/ajax/libs/localforage/1.9.0/localforage.min.js');
+
 self.addEventListener('install', event => {
   console.log('Service Worker installing.');
   // You can cache assets here if needed
@@ -7,33 +11,53 @@ self.addEventListener('activate', event => {
   console.log('Service Worker activating.');
 });
 
-self.addEventListener('push', event => {
-  console.log('Push event received:', event);
 
-  const data = event.data ? event.data.json() : {};
-  console.log('Push event data:', data);
-
-  const options = {
-    body: data.body || 'You have a new notification.',
-    icon: data.icon || '/icon.png',
-    data: { url: data.url || '/' }
-  };
-
-  event.waitUntil(
-    self.registration.showNotification(data.title || 'New Notification', options)
-  );
+self.addEventListener('fetch', (event) => {
+  // The default fetch handling is sufficient for share target
 });
 
-self.addEventListener('notificationclick', event => {
+const firebaseConfig = {
+  apiKey: "AIzaSyD96IBVqGKVEdmXIVCYL_7kvlBhJNSD1Ww",
+  authDomain: "marko-be9a9.firebaseapp.com",
+  databaseURL: "https://marko-be9a9-default-rtdb.firebaseio.com",
+  projectId: "marko-be9a9",
+  storageBucket: "marko-be9a9.appspot.com",
+  messagingSenderId: "7036670175",
+  appId: "1:7036670175:web:99992356716578ea13996a"
+};
+
+firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
+messaging.onBackgroundMessage(function(payload) {
+  console.log('Received background message ', payload);
+
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: payload.notification.icon || '/icon.png',
+    data: {
+      url: payload.notification.click_action || '/',
+    }
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
+
+self.addEventListener('notificationclick', function(event) {
   event.notification.close();
+
   event.waitUntil(
-    clients.matchAll({ type: 'window', includeUncontrolled: true }).then(clientList => {
+    clients.matchAll({ type: 'window', includeUncontrolled: true }).then(function(clientList) {
       if (clientList.length > 0) {
-        const client = clientList[0];
-        return client.navigate(event.notification.data.url).then(() => client.focus());
+        let client = clientList[0];
+        return client.focus().then(function() {
+          return client.navigate(event.notification.data.url);
+        });
+      } else {
+        return clients.openWindow(event.notification.data.url);
       }
-      return clients.openWindow(event.notification.data.url);
     })
   );
 });
+
 
